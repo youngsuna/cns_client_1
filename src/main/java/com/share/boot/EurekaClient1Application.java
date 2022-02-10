2fa9e5c5c6edf4e6ae7c3ebcd4b3ed7051cb4a0d
youngsuna <youngsunas2@gmail.com>
2022-02-10 15:45:41 +0900

    client1 : user-service client

--- src/main/java/com/share/boot/EurekaClient1Application.java
+++ src/main/java/com/share/boot/EurekaClient1Application.java
@@ -1,15 +1,23 @@
 package com.share.boot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class EurekaClient1Application {
+@RestController
+public class EurekaClient1Application {		//UserServiceApplication
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClient1Application.class, args);
 	}
 
+    @GetMapping("/user/info")
+    public String info(@Value("${server.port}") String port) {
+        return "User 서비스의 기본 동작 Port: {" + port + "}";
+    }
 }
